99c5c644c7a995e54f09efbc0b2cedaeaa592f8a
wanjunya.jiraiya <wanjunya.jiraiya@37474c4b-492e-061c-887a-49544c30887f>
2010-11-22 12:19:19 +0000

    修改了Login的部分代码,并增加了DebugClass类

---
+++ ducky project/src/object/DebugClass.java
@@ -0,0 +1,7 @@
+package object;
+
+public class DebugClass {
+	static public void debug_info(String _modle_name,String _info){
+		System.out.println("modle " + _modle_name + ": " + _info);
+	}
+}
--- ducky project/src/object/LoginUser_Manager.java
+++ ducky project/src/object/LoginUser_Manager.java
@@ -7,41 +7,74 @@ import dbquery.DBOperation;
 //用于在登录和注销的用例中进行数据库的查询和写入操作.
 public class LoginUser_Manager {
 	private static LoginQueryInfo sendLoginInfoToDB(LoginReqDB reqDb){
-		LoginQueryInfo tmpInfo = new LoginQueryInfo();
+		LoginQueryInfo queryInfo = new LoginQueryInfo();
 		
+		//连接数据库的参数
+		//要么存在一个全局数据集中,要么放在连接函数中
 		String driverName = "com.microsoft.sqlserver.jdbc.SQLServerDriver" ;
-		String url = "jdbc:sqlserver://192.168.0.140:1433; DatabaseName=J2EE_Course_Selecting_Sys" ;
+		String url = "jdbc:sqlserver://111.187.79.26:1433; DatabaseName=J2EE_Course_Selecting_Sys" ;
 		String userName = "j2eeServletUser" ;
-		String pwd = "321" ;
+		String dbPwd = "321" ;
 		
 		DBOperation dbo = new DBOperation() ;
-		dbo.connectDB(driverName, url, userName, pwd) ;
+		dbo.connectDB(driverName, url, userName, dbPwd) ;
 		
 		LoginUser lu = dbo.doLoginQuery(reqDb.getU_id()) ;
 		User user = dbo.doUserQuery(reqDb.getU_id()) ;
 		
 		if(lu != null){
-			dbo.doUpdateIsLogin(reqDb.getU_id(),true) ; // set the flag of login to be true 
-			tmpInfo.setU_pwd(lu.getU_pwd());
-			tmpInfo.setU_isLogin(true);
-			tmpInfo.setU_id(user.getU_id());
-			tmpInfo.setU_type(user.getU_type());
-			tmpInfo.setExp(null);
-		}else{
-			//id not exist!!!
-			tmpInfo.setU_isLogin(false);
+			//get the data from database to the query information
+			DebugClass.debug_info("Login modle", "connect the DB success!");
+			DebugClass.debug_info("Login modle", "get the user information!");
+			queryInfo.setU_pwd(lu.getU_pwd());
+			queryInfo.setU_id(user.getU_id());
+			queryInfo.setU_isLogin(lu.isU_isLogin());
+/*
+			if(lu.getU_pwd() == pwd){
+				queryInfo.setU_isLogin(true);
+			}else{
+				queryInfo.setU_isLogin(false);
+			}
+	*/		
+			queryInfo.setU_type(user.getU_type());
 		}
-
+		else{
+			//id not exist!!!
+			//queryInfo.setU_isLogin(false);
+			DebugClass.debug_info("Login modle", "no user");
+			queryInfo.setU_type(0);//0 stands for the null
+			queryInfo.setU_id(null);
+		}
+		//dbo.doUpdateIsLogin(reqDb.getU_id(),queryInfo.isU_isLogin()) ; // set the flag of login to be true 
 		dbo.disconnectDB();
-
-		return tmpInfo;
+		return queryInfo;
+	}
+	
+	private static LoginRltInfo LoginCheck(LoginInputInfo _input , LoginQueryInfo _query){
+		//察看是否存在用户
+		LoginRltInfo _rlt = new LoginRltInfo();
+		_rlt.setU_id(_query.getU_id());
+		if(_query.getU_id() == null){
+			//用户不存在的处理函式..
+			//_rlt.setExp("用户不存在!");
+			_rlt.setIs_succ(false);
+		}
+		else if(_query.getU_pwd() != _input.getU_pwd()){
+			//密码错误处理函式
+			_rlt.setIs_succ(false);
+		}
+		else{
+			//登陆正确
+			_rlt.setIs_succ(true);
+		}
+		return _rlt;
 	}
 	
 	private static LogoutQueryInfo sendLogoutInfoToDB(LogoutReqDB reqDb){
 		LogoutQueryInfo tmpInfo = new LogoutQueryInfo();
 		
 		String driverName = "com.microsoft.sqlserver.jdbc.SQLServerDriver" ;
-		String url = "jdbc:sqlserver://192.168.0.140:1433; DatabaseName=J2EE_Course_Selecting_Sys" ;
+		String url = "jdbc:sqlserver://111.187.79.26:1433; DatabaseName=J2EE_Course_Selecting_Sys" ;
 		String userName = "j2eeServletUser" ;
 		String pwd = "321" ;
 		
@@ -49,7 +82,10 @@ public class LoginUser_Manager {
 		dbo.connectDB(driverName, url, userName, pwd) ;
 		
 		LoginUser lu = dbo.doLoginQuery(reqDb.getU_id()) ;
-		dbo.doUpdateIsLogin(reqDb.getU_id(), false) ; // set the flag of login to be true 
+		
+		if(lu != null){
+			dbo.doUpdateIsLogin(reqDb.getU_id(), false) ; // set the flag of login to be true
+		}
 		
 		dbo.disconnectDB() ;
 		
@@ -63,15 +99,8 @@ public class LoginUser_Manager {
 	public static LoginRltInfo login(LoginInputInfo inputInfo){
 		LoginReqDB reqDb = new LoginReqDB();
 		reqDb.setU_id(inputInfo.getU_id());
-		
 		LoginQueryInfo queryInfo = sendLoginInfoToDB(reqDb);
-		
-		LoginRltInfo rltInfo = new LoginRltInfo();
-		rltInfo.setU_id(queryInfo.getU_id());
-		rltInfo.setU_type(queryInfo.getU_type());
-		rltInfo.setIs_succ(queryInfo.isU_isLogin());
-		rltInfo.setExp(queryInfo.getExp());
-		
+		LoginRltInfo rltInfo = LoginCheck(inputInfo,queryInfo);
 		return rltInfo;
 	}
 	
