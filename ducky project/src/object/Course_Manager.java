3f99864d3d81c1aa1f492c7c80df86281879d041
wanjunya.jiraiya <wanjunya.jiraiya@37474c4b-492e-061c-887a-49544c30887f>
2010-11-24 13:06:54 +0000

    class Course_Manager修改

--- ducky project/src/object/Course_Manager.java
+++ ducky project/src/object/Course_Manager.java
@@ -7,8 +7,8 @@ import object.Course;
 
 public class Course_Manager {
 	//取得所有课程信息列表,用于在预选界面进行显示
-	static public Vector getAllCourseList(){
-		Vector allCourseList = new Vector();
+	public Vector<Course> getAllCourseList(){
+		Vector<Course> allCourseList = new Vector<Course>();
 		Course tmpCourse = new Course();
 		
 		return allCourseList;
