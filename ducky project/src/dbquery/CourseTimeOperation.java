192f168eae8aafdec830c2b2676b1ed7706dcd19
wanjunya.jiraiya <wanjunya.jiraiya@37474c4b-492e-061c-887a-49544c30887f>
2010-11-29 13:51:05 +0000

    课程信息在数据库中以int存储，显示的时候需要转换，这个类就是这个功能

---
+++ ducky project/src/dbquery/CourseTimeOperation.java
@@ -0,0 +1,37 @@
+package dbquery;
+
+public class CourseTimeOperation
+{
+	private static final int MAX_NUM_OF_COURSES_PER_DAY = 11 ;
+	// should not larger than 25 ;
+	static public int convert2Day(int data)
+	{
+		int sample = 1 ;
+		sample  <<=  31 ;
+		
+		int day = 0 ;
+		for ( day = 0 ; day < 7 ; ++day  )
+		{
+			if  ( sample == ( data & sample ) )
+				break ;
+			data <<= 1 ;
+		}
+		
+		System.out.println("day : " + (day+1) ) ;
+		return day ;
+	}
+	static public boolean isConflict(int time1,int time2) 
+	{
+		if ( convert2Day(time1) != convert2Day(time2) )
+			return false ;
+		
+		int com = time1 & time2 ;
+		for ( int i = 0 ; i < MAX_NUM_OF_COURSES_PER_DAY ; ++i )
+		{
+			if ( 1 == ( 1 & com ) )
+				return true ;
+			com >>= 1 ;
+		}
+		return false ;
+	}
+}
