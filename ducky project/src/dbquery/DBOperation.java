012827427d268532254a672502490e53c59b87b4
wanjunya.jiraiya <wanjunya.jiraiya@37474c4b-492e-061c-887a-49544c30887f>
2010-11-29 12:27:09 +0000

    个人课表的查询db

--- ducky project/src/dbquery/DBOperation.java
+++ ducky project/src/dbquery/DBOperation.java
@@ -5,61 +5,50 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.Vector;
-import object.* ;
+import object.*;
 
-public class DBOperation
-{
+public class DBOperation {
 	/************* member variables ***************/
 	private Connection m_conn;
 
 	/************* member methods ****************/
-	public DBOperation()
-	{
+	public DBOperation() {
 	}
 
 	public DBOperation(String driverName, String url, String userName,
-			String pwd)
-	{
+			String pwd) {
 		connectDB(driverName, url, userName, pwd);
 	}
 
 	public boolean connectDB(String driverName, String url, String userName,
-			String pwd)
-	{
+			String pwd) {
 		m_conn = null;
-		try
-		{
+		try {
 			Class.forName(driverName);
 			m_conn = DriverManager.getConnection(url, userName, pwd);
 			m_conn.setAutoCommit(false);
 			return true;
-		} catch (Exception e)
-		{
+		} catch (Exception e) {
 			System.out.println("error : " + e);
 			return false;
 		}
 	}
 
-	public boolean disconnectDB()
-	{
-		try
-		{
+	public boolean disconnectDB() {
+		try {
 			m_conn.close();
 			m_conn = null;
 			return true;
-		} catch (Exception e)
-		{
+		} catch (Exception e) {
 			System.err.println("error" + e);
 			return false;
 		}
 	}
 
-	public LoginUser doLoginQuery(String uid)
-	{
+	public LoginUser doLoginQuery(String uid) {
 		LoginUser rtn = new LoginUser();
 
-		try
-		{
+		try {
 			String query_login = "SELECT * FROM TB_LOGIN WHERE U_ID=?";
 			PreparedStatement ps_login = m_conn.prepareStatement(query_login);
 			ps_login.setString(1, uid);
@@ -68,8 +57,7 @@ public class DBOperation
 			ResultSet results = ps_login.executeQuery();
 			/* do query */
 
-			while (results.next())
-			{
+			while (results.next()) {
 				rtn.setU_id(results.getString("U_ID"));
 				rtn.setU_pwd(results.getString("U_PWD"));
 				rtn.setU_isLogin(results.getBoolean("U_IS_LOGIN"));
@@ -79,20 +67,17 @@ public class DBOperation
 			/* close the resultset */
 			return rtn;
 
-		} catch (Exception e)
-		{
+		} catch (Exception e) {
 			System.err.println("error : " + e);
 			return null;
 		}
 	}
 
-	public User doUserQuery(String uid)
-	{
+	public User doUserQuery(String uid) {
 		User rtn = new User();
 		/* return value */
 
-		try
-		{
+		try {
 			String query_login = "SELECT * FROM TB_USER WHERE U_ID=?";
 			PreparedStatement ps_user = m_conn.prepareStatement(query_login);
 			ps_user.setString(1, uid);
@@ -100,8 +85,7 @@ public class DBOperation
 
 			ResultSet results = ps_user.executeQuery();
 			/* do query */
-			while (results.next())
-			{
+			while (results.next()) {
 				rtn.setU_id(results.getString("U_ID"));
 				rtn.setU_name(results.getString("U_NAME"));
 				rtn.setU_type(results.getInt("U_TYPE"));
@@ -114,18 +98,15 @@ public class DBOperation
 				/* close the resultset */
 				return rtn;
 			}
-		} catch (Exception e)
-		{
+		} catch (Exception e) {
 			System.err.println("error : " + e);
 			return null;
 		}
 		return null;
 	}
 
-	public void doUpdateIsLogin(String uid, boolean isLogin)
-	{
-		try
-		{
+	public void doUpdateIsLogin(String uid, boolean isLogin) {
+		try {
 			String query_login = "UPDATE TB_LOGIN SET U_IS_LOGIN=? WHERE U_ID=?";
 
 			PreparedStatement ps = m_conn.prepareStatement(query_login);
@@ -138,27 +119,23 @@ public class DBOperation
 			/* do update */
 			m_conn.commit();
 
-		} catch (Exception e)
-		{
+		} catch (Exception e) {
 			System.err.println("error : " + e);
 		}
 	}
 
-	public Vector<Course> doAllCourseQuery()
-	{
+	public Vector<Course> doAllCourseQuery() {
 		Vector<Course> rtn = new Vector<Course>();
-		try
-		{
+		try {
 			String query_course = "SELECT * FROM TB_COURSE ORDER BY COURSE_TYPE";
 			PreparedStatement ps = m_conn.prepareStatement(query_course);
 			/* some preparing work... */
-			
+
 			ResultSet results = ps.executeQuery();
-			while (results.next())
-			{
-				Course temp = new Course() ;
-				
-				temp.setCourse_id(results.getString("COURSE_ID")) ;
+			while (results.next()) {
+				Course temp = new Course();
+
+				temp.setCourse_id(results.getString("COURSE_ID"));
 				temp.setCourse_name(results.getString("COURSE_NAME"));
 				temp.setCourse_type(results.getInt("COURSE_TYPE"));
 				temp.setU_id(results.getString("U_ID"));
@@ -170,19 +147,91 @@ public class DBOperation
 				temp.setCourse_current_seleted_num(results
 						.getInt("COURSE_CURRENT_SELECTED_NUM"));
 				temp.setCourse_exam_type(results.getInt("COURSE_EXAM_TYPE"));
-				
-				rtn.add(temp) ;
+
+				rtn.add(temp);
 				/* write to rtn */
 			}
 			results.close();
 			/* close the resultset */
-			
-		} catch (Exception e)
-		{
-			System.err.println("error : " + e) ;
-			
+
+		} catch (Exception e) {
+			System.err.println("error : " + e);
+
 		}
 		return rtn;
+	}
+
+	private Vector<String> doQueryCourseID(String uid) {
+		Vector<String> rtn = new Vector<String>();
+
+		try {
+			String query_course_id = "SELECT COURSE_ID FROM TB_COURSE_SELECT WHERE U_ID=?";
+			PreparedStatement ps = m_conn.prepareStatement(query_course_id);
+			ps.setString(1, uid) ;
+			ResultSet results = ps.executeQuery();
+			
+			while(results.next())
+			{
+				rtn.add( results.getString("COURSE_ID") ) ;
+				/* write to rtn */
+			}
+			results.close();
+			/* close the resultset */
+		} catch (Exception e) {
+			System.err.println("error : " + e);
+		}
+
+		return rtn;
+	}
+	private Course doQueryCertainCourse(String course_id){
+		Course rtn = new Course() ;
+		
+		try {
+			String query_course = "SELECT * " +
+									 "FROM TB_COURSE " +
+									 "ORDER BY COURSE_TYPE " +
+									 "WHERE COURSE_ID=?" ;
+			PreparedStatement ps = m_conn.prepareStatement(query_course);
+			ps.setString(1, course_id) ;
+			ResultSet results = ps.executeQuery();
+			
+			while(results.next())
+			{
+				rtn.setCourse_id(results.getString("COURSE_ID"));
+				rtn.setCourse_name(results.getString("COURSE_NAME"));
+				rtn.setCourse_type(results.getInt("COURSE_TYPE"));
+				rtn.setU_id(results.getString("U_ID"));
+				rtn.setCourse_point(results.getInt("COURSE_POINT"));
+				rtn.setCourse_time(results.getInt("COURSE_TIME"));
+				rtn.setCourse_place(results.getString("COURSE_PLACE"));
+				rtn.setCourse_comment(results.getString("COURSE_COMMENT"));
+				rtn.setCourse_volume(results.getInt("COURSE_VOLUME"));
+				rtn.setCourse_current_seleted_num(results
+						.getInt("COURSE_CURRENT_SELECTED_NUM"));
+				rtn.setCourse_exam_type(results.getInt("COURSE_EXAM_TYPE"));
+
+				/* write to rtn */
+			}
+			results.close();
+			/* close the resultset */
+		} catch (Exception e) {
+			System.err.println("error : " + e);
+		}
+
+		return rtn;
+	}
+	public Vector<Course> doQueryPersonalCourse(String uid) {
+		Vector<Course> rtn = new Vector<Course>() ;
+		
+		Vector<String> course_id = doQueryCourseID(uid) ;
+		/*get all course id referred to this guy*/
+		
+		for ( int i = 0 ; i < course_id.size() ; ++i )
+		{
+			rtn.add(doQueryCertainCourse(course_id.elementAt(i))) ;
+		}
+		
+		return rtn ;
 	}
 
 }
