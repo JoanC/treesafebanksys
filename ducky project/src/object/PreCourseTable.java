55787f670a418280da84f28cfa425c47172996b6
wanjunya.jiraiya <wanjunya.jiraiya@37474c4b-492e-061c-887a-49544c30887f>
2010-12-01 11:02:07 +0000

    class PreCourseTable

---
+++ ducky project/src/object/PreCourseTable.java
@@ -0,0 +1,70 @@
+package object;
+
+
+import java.util.Vector;
+import object.CourseTable;
+import object.Exp;
+
+
+public class PreCourseTable extends CourseTable {
+	String u_id; 				//学生的id
+	Vector<Course> course_list; //课程表中的课程数据容器
+	
+	//学生或者老师的用户id的记录
+	public void set_u_id(String uid){
+		u_id = uid;
+	}
+	
+	//学生或者老师的用户id的获取
+	public String get_u_id(){
+		return u_id;
+	}
+	
+	//这个课表中的所有课程信息列表的记录
+	public Vector<Course> get_course_list(){
+		Vector<Course> course_list = null;
+		//...
+		return course_list;
+	}
+	
+	//在初始化时获取这个u_id下的课表中的所有课程列表
+	protected void searchCourseList(){
+		//...
+		course_list = null;	//给course_list赋值
+	}
+	
+	//预选数据的自动保存,这个在实现预选课表的add和delete方法时调用
+	private Exp altoSave(){
+		Exp exp = null;
+		//...
+		return exp;
+	}
+	
+	/*//将课程信息从预选课表中移出
+	public Exp moveCourseFromPreTable(Course _move){
+		Exp exp = null;
+		//...
+		return exp;
+	}
+	
+	//将课程信息移入预选课表中
+	public Exp moveCourseToPreTable(Course _move){
+		Exp exp = null;
+		//...
+		return exp;
+	}*/
+	
+	//将一门课程从选课初始的大列表选入到课程表中
+	public Exp addCourse(Course _new){
+		Exp exp = null;
+		//...
+		return exp;
+	}
+	
+	//将一门课程从预选课程表中删除
+	public Exp deleteCourse(Course _old){
+		Exp exp = null;
+		//...
+		return exp;
+	}
+}
