54d907563c955ba5663232bec29c33c74ac6e0bd
wanjunya.jiraiya <wanjunya.jiraiya@37474c4b-492e-061c-887a-49544c30887f>
2010-12-14 14:01:55 +0000

    add fctn CourseTimeOperation::convert2Detail()

--- ducky project/src/dbquery/CourseTimeOperation.java
+++ ducky project/src/dbquery/CourseTimeOperation.java
@@ -1,50 +1,54 @@
 package dbquery;
 
-import java.util.ArrayList;
-
-
+import java.util.Vector;
 
 public class CourseTimeOperation
 {
-	private static final int MAX_NUM_OF_COURSES_PER_DAY = 11 ;
+	private static final int MAX_NUM_OF_COURSES_PER_DAY = 11;
+
 	// should not larger than 25 ;
-	static public int convert2Day(int data)
+	static public int convert2Day(int time)
 	{
-		int sample = 1 ;
-		sample  <<=  31 ;
-		
-		int day = 0 ;
-		for ( day = 0 ; day < 7 ; ++day  )
+		int sample = 1;
+		sample <<= 31;
+
+		int day = 0;
+		for (day = 0; day < 7; ++day)
 		{
-			if  ( sample == ( data & sample ) )
-				break ;
-			data <<= 1 ;
+			if (sample == (time & sample))
+				break;
+			time <<= 1;
 		}
-		
-		System.out.println("day : " + (day+1) ) ;
-		return day ;
+
+		System.out.println("day : " + (day + 1));
+		return day;
 	}
-	static public boolean isConflict(int time1,int time2) 
+
+	static public boolean isConflict(int time1, int time2)
 	{
-		if ( convert2Day(time1) != convert2Day(time2) )
-			return false ;
-		
-		int com = time1 & time2 ;
-		for ( int i = 0 ; i < MAX_NUM_OF_COURSES_PER_DAY ; ++i )
+		if (convert2Day(time1) != convert2Day(time2))
+			return false;
+
+		int com = time1 & time2;
+		for (int i = 0; i < MAX_NUM_OF_COURSES_PER_DAY; ++i)
 		{
-			if ( 1 == ( 1 & com ) )
-				return true ;
-			com >>= 1 ;
+			if (1 == (1 & com))
+				return true;
+			com >>= 1;
 		}
-		return false ;
+		return false;
 	}
-	static public ArrayList CourseTimeOperation() 
+
+	static public Vector<Boolean> Convert2Detail(int time)
 	{
-		ArrayList rtn = new ArrayList() ;
-		for ( int i = MAX_NUM_OF_COURSES_PER_DAY ; i > 0 ; --i )
+		Vector<Boolean> rtn = new Vector<Boolean>();
+		int sample = 1;
+		for (int i = MAX_NUM_OF_COURSES_PER_DAY ; i > 0 ; --i )
 		{
-			
+			Boolean buf = new Boolean((sample & time) == sample) ;
+			rtn.addElement(buf) ;
+			sample <<= 1 ;
 		}
-		return rtn ;
+		return rtn;
 	}
 }
